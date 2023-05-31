daed2bd6f29d6c8f641c8dceae152c2473565f89
Arthur Henrique <130499233+ArthurH0@users.noreply.github.com>
2023-05-31 08:41:04 -0300

    Add files via upload

---
+++ CodeConnect/static/code/home.js
@@ -0,0 +1,16 @@
+
+/*function teste()
+    {
+     const num = 0;
+     let p  = document.getElementById('bver1')
+     alert(num);
+     console.log(`${num}`) ;
+}*/
+
+function teste(elemento){
+    var f = elemento.id;
+
+    if(f == 'bver1'){
+        alert(f)
+    }
+}
