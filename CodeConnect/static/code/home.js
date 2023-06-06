bd255124835a0984d6ba839cab880af6cbc52a36
Arthur Henrique <130499233+ArthurH0@users.noreply.github.com>
2023-06-06 20:05:46 -0300

    Add files via upload

--- CodeConnect/static/code/home.js
+++ CodeConnect/static/code/home.js
@@ -1,16 +1,33 @@
 
-/*function teste()
-    {
-     const num = 0;
-     let p  = document.getElementById('bver1')
-     alert(num);
-     console.log(`${num}`) ;
-}*/
-
-function teste(elemento){
+function teste(elemento) {
     var f = elemento.id;
+    var conteudo = document.getElementById("parag")
+    var texto = "";
 
-    if(f == 'bver1'){
+    if (f == "bver1") {
         alert(f)
+
+        var produtoatual =
+        `<div class="row">
+          <div class="col-12">
+            <p>oi</oi>
+          </div>
+        </div>`;
+    
+   texto += produtoatual
+   
+    } else if( f == 'bver2'){
+        alert(f)
+    } else if( f == 'bver3'){
+        alert(f)
+    }else if ( f == 'bver4'){
+        alert(f)
+    }else if ( f == 'bver5'){
+        alert(f)
+    }else if (f == 'bver6'){
+
     }
+
+    conteudo.innerHTML += texto;
+
 }
